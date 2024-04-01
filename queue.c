cd6caa38c6299cbd7e2efa791a7a935f4aa2dda8
Sam Kao <sam8409044601@gmail.com>
2024-04-01 14:52:09 +0800

    Implement q_delete_duplicate()
    If 'head' is NULL or the queue is empty, return false.
    Use 'list_for_each_entry_safe' to trace strings of nodes,
    and create a flag to describe it find out duplicates or not.

--- queue.c
+++ queue.c
@@ -118,7 +118,27 @@ bool q_delete_mid(struct list_head *head)
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    /* for base case */
+    if (!head || list_empty(head))
+        return false;
+
+    /* 1. use list_for_each_entry_safe to test whether there are duplicate
+     * nodes. */
+    element_t *entry;
+    element_t *safe;
+
+    /* 2. use the flag to check out the status. */
+    bool dup = false;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        if (&safe->list != head && !strcmp(entry->value, safe->value)) {
+            list_del(&entry->list);
+            q_release_element(entry);
+            dup = true;
+        } else if (dup) {
+            list_del(&entry->list);
+            dup = false;
+        }
+    }
     return true;
 }
 
